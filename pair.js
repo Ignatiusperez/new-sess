330cbff5d72da9baeb9d3202c6490889a82026df
Ignatiusperez <IgnatiusPerez86@gmail.com>
2025-03-31 22:19:19 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -93,9 +93,9 @@ router.get('/', async (req, res) => {
                         contextInfo: {
                             mentionedJid: [Pair_Code_By_Brasho_Kish.user.id], // Mention the sender in the audio message
                             externalAdReply: {
-                                title: 'Thanks for choosing 𝗞𝗲𝗶𝘁𝗵 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 happy deployment 💜',
+                                title: 'Thanks for choosing blackmachant happy deployment 💜',
                                 body: 'Regards Keithkeizzah',
-                                thumbnailUrl: 'https://i.imgur.com/vTs9acV.jpeg',
+                                thumbnailUrl: 'https://i.imgur.com/aGmtrh0.jpeg',
                                 sourceUrl: 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47',
                                 mediaType: 1,
                                 renderLargerThumbnail: true,
