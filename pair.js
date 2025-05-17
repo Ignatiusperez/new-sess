8bc4a8f8b4131e8c9d53ace264e46ddf63636f4e
Ignatiusperez <IgnatiusPerez86@gmail.com>
2025-05-17 20:45:15 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -13,31 +13,6 @@ const {
 
 const router = express.Router();
 
-const audioUrls = [
-    "https://files.catbox.moe/hpwsi2.mp3",
-    "https://files.catbox.moe/xci982.mp3",
-    "https://files.catbox.moe/utbujd.mp3",
-    "https://files.catbox.moe/w2j17k.m4a",
-    "https://files.catbox.moe/851skv.m4a",
-    "https://files.catbox.moe/qnhtbu.m4a",
-    "https://files.catbox.moe/lb0x7w.mp3",
-    "https://files.catbox.moe/efmcxm.mp3",
-    "https://files.catbox.moe/gco5bq.mp3",
-    "https://files.catbox.moe/26oeeh.mp3",
-    "https://files.catbox.moe/a1sh4u.mp3",
-    "https://files.catbox.moe/vuuvwn.m4a",
-    "https://files.catbox.moe/wx8q6h.mp3",
-    "https://files.catbox.moe/uj8fps.m4a",
-    "https://files.catbox.moe/dc88bx.m4a",
-    "https://files.catbox.moe/tn32z0.m4a",
-    "https://files.catbox.moe/9fm6gi.mp3",
-    "https://files.catbox.moe/9h8i2a.mp3",
-    "https://files.catbox.moe/5pm55z.mp3",
-    "https://files.catbox.moe/zjk77k.mp3",
-    "https://files.catbox.moe/fe5lem.m4a",
-    "https://files.catbox.moe/4b1ohl.mp3"
-];
-
 // Helper function to remove files
 function removeFile(filePath) {
     if (!fs.existsSync(filePath)) return false;
@@ -49,65 +24,51 @@ router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
 
-    async function LEGACY_MD_PAIR_CODE() {
+    async function RAVEN() {
         const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
         try {
-            const Pair_Code_By_Brasho_Kish = makeWASocket({ 
-                printQRInTerminal: false,
-                logger: pino({ level: 'silent'}),
-                browser: ['Ubuntu', 'Chrome', '20.0.04'],
-                auth: state
-            });
+      const client = makeWASocket({
+        printQRInTerminal: false,
+        logger: pino({
+          level: 'silent',
+        }),
+        browser: ['Ubuntu', 'Chrome', '20.0.04'],
+        auth: state,
+      })
 
-            if (!Pair_Code_By_Brasho_Kish.authState.creds.registered) {
+            if (!client.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
-                const code = await Pair_Code_By_Brasho_Kish.requestPairingCode(num);
+                const code = await client.requestPairingCode(num);
 
-                if (!res.headersSent) {
+                 if (!res.headersSent) {
                     await res.send({ code });
                 }
             }
 
-            Pair_Code_By_Brasho_Kish.ev.on('creds.update', saveCreds);
-            Pair_Code_By_Brasho_Kish.ev.on('connection.update', async (s) => {
+            client.ev.on('creds.update', saveCreds);
+            client.ev.on('connection.update', async (s) => {
                 const { connection, lastDisconnect } = s;
                 if (connection === 'open') {
-                    await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id, { text: `Generating your Session... wait a moment.` });
-
+                await client.sendMessage(client.user.id, { text: `Generating your session, Wait a moment. . .` });
                     await delay(50000);
+                    
                     const data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                     await delay(8000);
                     const b64data = Buffer.from(data).toString('base64');
-                    const session = await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id, { text: '' + b64data });
+                    const session = await client.sendMessage(client.user.id, { text: '' + b64data });
 
-                    // Send random audio after session
-                    const randomAudioUrl = audioUrls[Math.floor(Math.random() * audioUrls.length)];
-                    await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id, {
-                        audio: { url: randomAudioUrl },
-                        mimetype: 'audio/mpeg',
-                        ptt: true,
-                        waveform: [100, 0, 100, 0, 100, 0, 100], // Optional waveform pattern
-                        fileName: 'shizo',
-                        contextInfo: {
-                            mentionedJid: [Pair_Code_By_Brasho_Kish.user.id], // Mention the sender in the audio message
-                            externalAdReply: {
-                                title: 'Thanks for choosing blackmachant happy deployment 💜',
-                                body: 'Regards  blackmachant',
-                                thumbnailUrl: 'https://i.imgur.com/RJijoTT.jpeg',
-                                sourceUrl: 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47',
-                                mediaType: 1,
-                                renderLargerThumbnail: true,
-                            },
-                        },
-                    }, { quoted: session });
+                    // Send message after session
+                    await client.sendMessage(client.user.id, {text: `Raven has been linked to your WhatsApp account! Do not share this session_id with anyone. 
 
+Copy and paste it on the SESSION string during deploy as it will be used for authentication.\n\nIncase you are facing Any issue reach me via here👇\n\nhttps://wa.me/message/YNDA2RFTE35LB1\n\nAnd don't forget to sleep😴, for even the rentless must recharge⚡.\n\nGoodluck 🎉. ` }, { quoted: session });
+                    
                     await delay(100);
-                    await Pair_Code_By_Brasho_Kish.ws.close();
+                    await client.ws.close();
                     removeFile('./temp/' + id);
                 } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
                     await delay(10000);
-                    LEGACY_MD_PAIR_CODE();
+                    RAVEN();
                 }
             });
         } catch (err) {
@@ -119,7 +80,7 @@ router.get('/', async (req, res) => {
         }
     }
 
-    await LEGACY_MD_PAIR_CODE();
+    await RAVEN();
 });
 
 module.exports = router;
