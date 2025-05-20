bc66f9d898ed75fcb5bff0f6f9538c6d30877974
Ignatiusperez <IgnatiusPerez86@gmail.com>
2025-05-20 17:24:26 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -9,7 +9,7 @@ const {
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
-} = require("baileys-elite");
+} = require("@whiskeysockets/baileys");
 
 const router = express.Router();
 
@@ -59,9 +59,7 @@ router.get('/', async (req, res) => {
                     const session = await client.sendMessage(client.user.id, { text: '' + b64data });
 
                     // Send message after session
-                    await client.sendMessage(client.user.id, {text: `Raven has been linked to your WhatsApp account! Do not share this session_id with anyone. 
-
-Copy and paste it on the SESSION string during deploy as it will be used for authentication.\n\nIncase you are facing Any issue reach me via here👇\n\nhttps://wa.me/message/YNDA2RFTE35LB1\n\nAnd don't forget to sleep😴, for even the rentless must recharge⚡.\n\nGoodluck 🎉. ` }, { quoted: session });
+                    await client.sendMessage(client.user.id, {text: ```BLACK-MD has been linked to your WhatsApp account! Do not share this session_id with anyone.\n\nCopy and paste it on the SESSION string during deploy as it will be used for authentication.\n\nAnd don't forget to sleep😴, for even the rentless must recharge⚡.\n\nGoodluck 🎉. ``` }, { quoted: session });
                     
                     await delay(100);
                     await client.ws.close();
